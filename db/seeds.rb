f9dfabbfab45dfd4114575f21071a025a50587ed
menja-coder <rakotomenja@gmail.com>
2019-08-15 15:46:57 +0300

    MAJ dB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Attendance.destroy_all
+Event.destroy_all
 User.destroy_all
+
 
 require 'faker'
 
-10.times do
-  user = User.create!( email:"#{Faker::Name.first_name}@yopmail.com",
-                       encrypted_password:Faker::Internet.password,
-  	                   description:Faker::TvShows::HowIMetYourMother.quote,
-  	                   first_name:Faker::Name.first_name , 
-  	                   last_name:Faker::Name.last_name 
-                 
-  	                    )
+ 2.times do
+
+    user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description:Faker::TvShows::HowIMetYourMother.quote, 
+
+        email:"#{Faker::Name.first_name}@yopmail.com", encrypted_password: Faker::Internet.password)
+
+    user.save
+
 end
-puts "user "
 
+puts "User ok"
 
+20.times do
+
+    event = Event.create( title: "Mon super event ",
+
+                    description: "La description super event",
+
+                    price: rand(1...1000),
+
+                    location: Faker::Address.city,
+
+                    start_date: Faker::Date.forward,
+
+                    duration: 5*rand(1..120),
+
+                    admin: User.all.sample)
+
+end
+
+puts "Event added"
+
+10.times do
+
+    attendance = Attendance.create(stripe_customer_id: Faker::Lorem.characters, event: Event.all.sample, participant: User.all.sample)
+
+    attendance.save
+
+end
+
+puts "Attendance created"
 
 
 
