250108251cfefd39e38b3cd8a350ca8f31ca9e93
chris241 <chrisleon325@gmail.com>
2019-08-20 13:14:02 +0300

    premier heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_065837) do
+ActiveRecord::Schema.define(version: 2019_08_20_100803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(version: 2019_08_14_065837) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password"
-    t.text "description"
     t.string "first_name"
     t.string "last_name"
+    t.text "description"
+    t.string "email"
+    t.string "encrypted_password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
